685366579567f4289f8b4554bbcaaf4e050edfb6
Kenil Kavar <125490448+Kenil-kavar@users.noreply.github.com>
2023-07-12 19:57:52 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId=144553
+let accountEmail ="kenil@gmail.com"
+var accountCity="jaipur"
+let accountstate;
+
+// accountId =2 // not allowed
+
+accountEmail =="kk@en.com"
+accountPassword = "212121"
+accountCity = "Bngaluru"
+
+/*
+Prefer not to use var
+Because of issur in block  scope  and functional scope
+*/
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountCity, accountPassword, accountstate])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code e as new version
+
+// alert (3+3 )// we are using node.js, not browser
+
+console.log(3+3)  // code readability
+console.log("Kenil")
+
+let name="kenil"
+let age=19
+let isLoggedIn = false
+
+//number => 2 to  power 53
+// big int
+// string =>""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+console.log(typeof kenil)
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isLoggedIn)
+console.log(typeof null) // Here in JS null is  a object
