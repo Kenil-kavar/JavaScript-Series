01482cfd959168172633de7b6f6bd3882bf9f15b
Kenil Kavar <125490448+Kenil-kavar@users.noreply.github.com>
2023-07-17 10:34:11 +0000

    Conversions of Datatypes

---
+++ 01_basics/03_conversionsOperation.js
@@ -0,0 +1,20 @@
+let score="Kenil"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber= Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+console.log(score)
+
+
+//"33">= 33
+//"33acd">=NaN
+// true>=1, false>=0
+//""=> false
+// "Kenil"=>true
+let somenummber=33
+let Stringnumber=String(somenummber)
+console.log(Stringnumber)
+console.log(typeof Stringnumber)
