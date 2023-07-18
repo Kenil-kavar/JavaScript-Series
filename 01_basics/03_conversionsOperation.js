8a37ef3105f0fa28b58cdaa4990e7a4b6cf3ee81
Kenil Kavar <125490448+Kenil-kavar@users.noreply.github.com>
2023-07-18 13:42:14 +0000

    comparision and data summary

--- 01_basics/03_conversionsOperation.js
+++ 01_basics/03_conversionsOperation.js
@@ -18,3 +18,27 @@ let somenummber=33
 let Stringnumber=String(somenummber)
 console.log(Stringnumber)
 console.log(typeof Stringnumber)
+
+//----------Operations----------
+
+let value=3
+let negativeValue=-value
+console.log(negativeValue)
+console.log(2+2)
+
+console.log("1"+2)
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+
+
+console.log(+true)
+console.log(+"")
+
+let num1, num2, num3;
+
+num1=num2=num3=2+3;
+
+let GameCounter=100;
+GameCounter++;
+console.log(GameCounter);
