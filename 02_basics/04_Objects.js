c46d26437233fdee62c5168ede2e0ee2183163fe
Kenil Kavar <125490448+Kenil-kavar@users.noreply.github.com>
2023-08-08 18:07:58 +0530

    04_Objects.js

---
+++ 02_basics/04_Objects.js
@@ -0,0 +1,55 @@
+const tinderUser= new Object(); // Here this is a singleton object.
+const tinderUser1= {};         // here this is a non singleton object.
+tinderUser1.id=="123abc";
+tinderUser1.name="sammy";
+tinderUser1.isLogedIn= false;
+
+
+//console.log(tinderUser);
+//console.log(tinderUser1);
+
+const regularUser={
+    email:"some@gail.com",
+    fullname:{
+        userfullname:{
+            firstname:"Kenil",
+            lastname:"Kavar",
+
+        }
+    }
+}
+console.log(regularUser.fullname.userfullname.firstname);
+console.log(regularUser.fullname.userfullname.lastname);
+
+const obj1= {1:"a",2:"b"}
+const obj2= {3:"a",4:"b"}
+
+//const obj3={obj1,obj2};
+//const obj3= Object.assign({},obj1,obj2);// Here the {} are working as an empty objects. 
+//the value of object 1 and 2 targets the {} and then transfer it to the obj3.
+//console.log(obj3);
+//const obj3= {...obj1,...obj2};// Here ... Operator is working as an spread operator
+//console.log(obj3);
+
+const users=[
+    {
+        id:1,
+        email:"h@gmail.com"
+    },
+    {
+        id:1,
+        email:"h@gmail.com"
+    },
+    {
+        id:1,
+        email:"h@gmail.com"
+    }
+]
+users[1].email
+console.log(tinderUser1)
+
+console.log(Object.keys(tinderUser1))
+console.log(Object.values(tinderUser1))
+console.log(Object.entries(tinderUser1))
+
+console.log(tinderUser1.hasOwnProperty("isLogedIn"));
